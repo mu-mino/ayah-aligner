3342e1665242baf7a3ba0682bbd700737c95b6cc
Muhammed Emin Eser <ebotexo3@gmail.com>
2026-04-08 08:45:20 +0200

    perf(asyncio): wait for first completed process instead of all

--- auto_range_runner.py
+++ auto_range_runner.py
@@ -47,13 +47,24 @@ async def main():
 
         process = await run_process(text_file, audio, video, i)
         processes.append(process)
-
+        print(f"{'#'*20} RUNNING COMMAND: {'#'*20}")
+        print(
+            f"python main.py --video '{video}' --text '{text_file}' --surah '{i}' --audio '{audio}'"
+        )
+        print("#" * 58)
         print(f"running process with id {i}")
+        print("#" * 58)
 
         is_last = i == end_id - 1
         if len(processes) == 13 or (is_last and processes):
-            await asyncio.gather(*[p.wait() for p in processes])
-            processes = []
+            tasks = {asyncio.create_task(p.wait()): p for p in processes}
+            done, pending = await asyncio.wait(
+                tasks.keys(), return_when=asyncio.FIRST_COMPLETED
+            )
+            for task in done:
+                proc = tasks.pop(task)
+                processes.remove(proc)
+                print(f"Prozess fertig: PID={proc.pid}")
 
 
 asyncio.run(main())
